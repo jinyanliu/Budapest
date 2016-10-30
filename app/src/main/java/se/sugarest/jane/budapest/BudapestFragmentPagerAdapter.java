2f51cfb24546584202de9d4fb6d4a6840c600302
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-10-30 14:39:49 +0100

    Set up app structure. Add TabLayout and ViewPager in activity_main xml layout file. Add BudapestFragmentPagerAdapter java class.

---
+++ app/src/main/java/se/sugarest/jane/budapest/BudapestFragmentPagerAdapter.java
@@ -0,0 +1,65 @@
+package se.sugarest.jane.budapest;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by jane on 16-10-30.
+ */
+
+public class BudapestFragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
+
+    /**
+     * Context of the app
+     */
+    private Context mContext;
+
+    /**
+     * Create a new BudapestFragmentPagerAdapter object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter across swipes.
+     */
+    public BudapestFragmentPagerAdapter(Context context, FragmentManager fm) {
+
+        super(fm);
+        mContext = context;
+    }
+
+    /**
+     * @param position
+     * @return the Fragment that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new EatFragment();
+        } else if (position == 1) {
+            return new PlayFragment();
+        } else if (position == 2) {
+            return new SeeFragment();
+        } else
+            return new ShopFragment();
+    }
+
+    /**
+     * @return the total number of pages.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_see);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_eat);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_shop);
+        } else {
+            return mContext.getString(R.string.category_play);
+        }
+    }
+}
