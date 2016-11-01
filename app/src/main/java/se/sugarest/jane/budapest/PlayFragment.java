0f1bc57329552d804760889649e832c4534b65c2
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-11-01 14:18:48 +0100

    Add booking time for eat_meat_boutique. Add Kiosk also for Play.

--- app/src/main/java/se/sugarest/jane/budapest/PlayFragment.java
+++ app/src/main/java/se/sugarest/jane/budapest/PlayFragment.java
@@ -26,6 +26,7 @@ public class PlayFragment extends Fragment {
         //Create an array list of places.
         ArrayList<Place> places = new ArrayList<Place>();
 
+        places.add(new Place("Kiosk (Dec 27 19:00)","A meeting point where you will find the perfect mood adjusted for every time of the day.","http://kiosk-budapest.com/", R.drawable.eat_kiosk));
         places.add(new Place("Magnolia Day Spa","day spa","http://www.magnoliadayspa.hu/", 0));
         places.add(new Place("Mandala Day Spa","day spa","http://www.en.mandaladayspa.hu/", 0));
         places.add(new Place("Széchenyi thermal bath","bath","http://www.szechenyibath.hu/", 0));
