f83357746087d9d67689a3e3c1ea634f2953f2bf
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-11-02 12:21:25 +0100

    Add shop_plaza_arena

--- app/src/main/java/se/sugarest/jane/budapest/ShopFragment.java
+++ app/src/main/java/se/sugarest/jane/budapest/ShopFragment.java
@@ -26,12 +26,10 @@ public class ShopFragment extends Fragment {
         //Create an array list of places.
         ArrayList<Place> places = new ArrayList<Place>();
 
-        places.add(new Place("Premier Outlets","30-70% off normal retail price everyday!","http://premieroutlet.hu/", R.drawable.shop_outlet));
-        places.add(new Place("CBA","Supermarket","http://www.cba.hu/", R.drawable.shop_cba));
         places.add(new Place("Terminal Market","Authentic wines from the hearth of Europe.","http://terminalrestaurant.hu/", R.drawable.shop_terminal_market));
         places.add(new Place("Café Gerbeaud","You can choose from an extensive range of beautifully and exquisitely packaged chocolate.","http://www.gerbeaud.hu/", R.drawable.shop_gerbeaud_cukraszda));
         places.add(new Place("Book Café - Lotz Hall","Not only bookstores, multi-purpose cultural center as well.","http://www.lotzterem.hu/", R.drawable.shop_book_store));
-        places.add(new Place("Arena Plaza","shopping plaza","http://www.arenaplaza.hu/en/", 0));
+        places.add(new Place("Arena Plaza","Great Entertainment.","http://www.arenaplaza.hu/", R.drawable.shop_plaza_arena));
         places.add(new Place("Váci Street","tourist central","https://en.wikipedia.org/wiki/V%C3%A1ci_Street", 0));
         places.add(new Place("Andrássy út","shopping street","https://en.wikipedia.org/wiki/Andr%C3%A1ssy_%C3%BAt", 0));
         places.add(new Place("Hecserli","food","http://www.hecserli.hu/", 0));
@@ -41,6 +39,8 @@ public class ShopFragment extends Fragment {
         places.add(new Place("Vass Shoes","shoes","http://www.vass-shoes.com/", 0));
         places.add(new Place("Bortársaság","wine","http://www.bortarsasag.hu/en/", 0));
         places.add(new Place("BÁV","antiques","http://www.bav.hu/en/", 0));
+        places.add(new Place("Premier Outlets","30-70% off normal retail price everyday!","http://premieroutlet.hu/", R.drawable.shop_outlet));
+        places.add(new Place("CBA","Supermarket","http://www.cba.hu/", R.drawable.shop_cba));
 
         /**
          * Create a PlaceAdapter, whose data source is a list of Places.
