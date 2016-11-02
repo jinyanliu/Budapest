f66a971dde785213132154698d7181b2d99161a3
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-11-02 15:20:38 +0100

    Add play_magnolia_day_spa

--- app/src/main/java/se/sugarest/jane/budapest/PlayFragment.java
+++ app/src/main/java/se/sugarest/jane/budapest/PlayFragment.java
@@ -30,7 +30,7 @@ public class PlayFragment extends Fragment {
         places.add(new Place("Széchenyi thermal bath", "One of Europe's largest bath complexes in the heart of the City Park.", "http://www.szechenyifurdo.hu/", R.drawable.play_szechenyi_thermal_bath));
         places.add(new Place("Gellért Baths", "Part of the famous Hotel Gellért in Buda, which is a bath complex.", "http://www.gellertfurdo.hu/", R.drawable.play_gellert_baths));
         places.add(new Place("Rudas Baths", "A thermal and medicinal bath.", "http://www.rudasfurdo.hu/", R.drawable.play_rudas_baths));
-        places.add(new Place("Magnolia Day Spa", "day spa", "http://www.magnoliadayspa.hu/", 0));
+        places.add(new Place("Magnolia Day Spa", "A place where no effort is spared to achieve harmony of body and soul.", "http://www.magnoliadayspa.hu/", R.drawable.play_magnolia_day_spa));
         places.add(new Place("Mandala Day Spa", "day spa", "http://www.en.mandaladayspa.hu/", 0));
         places.add(new Place("OXYGEN Wellness", "gym & bath", "http://www.oxygenwellness.hu/", 0));
         places.add(new Place("Buda Hills", "hill", "https://en.wikipedia.org/wiki/Buda_Hills", 0));
