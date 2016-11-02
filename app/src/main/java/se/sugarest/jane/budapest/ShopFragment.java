6129a2a1dfef5f844403919c85a44ffd96799d76
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-11-02 12:11:13 +0100

    Add shop_cba

--- app/src/main/java/se/sugarest/jane/budapest/ShopFragment.java
+++ app/src/main/java/se/sugarest/jane/budapest/ShopFragment.java
@@ -26,10 +26,11 @@ public class ShopFragment extends Fragment {
         //Create an array list of places.
         ArrayList<Place> places = new ArrayList<Place>();
 
+        places.add(new Place("Premier Outlets","30-70% off normal retail price everyday!","http://premieroutlet.hu/", R.drawable.shop_outlet));
+        places.add(new Place("CBA","Supermarket","http://www.cba.hu/", R.drawable.shop_cba));
         places.add(new Place("Terminal Market","Authentic wines from the hearth of Europe.","http://terminalrestaurant.hu/", R.drawable.shop_terminal_market));
         places.add(new Place("Café Gerbeaud","You can choose from an extensive range of beautifully and exquisitely packaged chocolate.","http://www.gerbeaud.hu/", R.drawable.shop_gerbeaud_cukraszda));
         places.add(new Place("Book Café - Lotz Hall","Not only bookstores, multi-purpose cultural center as well.","http://www.lotzterem.hu/", R.drawable.shop_book_store));
-        places.add(new Place("Premier Outlets","30-70% off normal retail price everyday!","http://premieroutlet.hu/", R.drawable.shop_outlet));
         places.add(new Place("Arena Plaza","shopping plaza","http://www.arenaplaza.hu/en/", 0));
         places.add(new Place("Váci Street","tourist central","https://en.wikipedia.org/wiki/V%C3%A1ci_Street", 0));
         places.add(new Place("Andrássy út","shopping street","https://en.wikipedia.org/wiki/Andr%C3%A1ssy_%C3%BAt", 0));
