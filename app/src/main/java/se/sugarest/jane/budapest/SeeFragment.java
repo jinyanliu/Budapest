d90133c7d2241a9a7538d17f817f1bf6ced9151b
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-11-02 17:44:45 +0100

    Add see_fishermans_bastion, see_matthias_church

--- app/src/main/java/se/sugarest/jane/budapest/SeeFragment.java
+++ app/src/main/java/se/sugarest/jane/budapest/SeeFragment.java
@@ -31,14 +31,16 @@ public class SeeFragment extends Fragment {
         places.add(new Place("Hungarian National Gallery", "Established in 1957 as the national art museum.", "http://mng.hu/", R.drawable.see_hungarian_national_gallery));
         places.add(new Place("National Széchényi Library", "Repositories of knowledge in the cultural heritage.", "http://www.oszk.hu/", R.drawable.see_national_szechenyi_library));
         places.add(new Place("Museum of Fine Arts", "International Art", "http://www.szepmuveszeti.hu/", R.drawable.see_museum_of_fine_arts));
-        places.add(new Place("Fisherman's Bastion", "historical building", "http://www.fishermansbastion.com/", 0));
+        places.add(new Place("Fisherman's Bastion", "Were built in the 19th century to serve as a lookout tower for the best panoramic views.", "http://www.fishermansbastion.com/", R.drawable.see_fishermans_bastion));
+        places.add(new Place("Matthias Church", "A Roman Catholic church, which was constructed in the florid late Gothic style.", "http://www.matyas-templom.hu/", R.drawable.see_matthias_church));
+        places.add(new Place("Chain Bridge", "bridge", "https://en.wikipedia.org/wiki/Chain_Bridge_(Budapest)", 0));
+
 
         places.add(new Place("Budapest Zoo and Botanical Garden", "zoo & park", "https://en.wikipedia.org/wiki/Budapest_Zoo_and_Botanical_Garden", 0));
         places.add(new Place("Margaret Island", "island & park", "https://en.wikipedia.org/wiki/Margaret_Island", 0));
-        places.add(new Place("Chain Bridge", "bridge", "https://en.wikipedia.org/wiki/Chain_Bridge_(Budapest)", 0));
+
         places.add(new Place("Deák Ferenc square", "square & transport junction", "https://en.wikipedia.org/wiki/De%C3%A1k_Ferenc_t%C3%A9r", 0));
         places.add(new Place("Great Market Hall", "indoor market", "https://en.wikipedia.org/wiki/Great_Market_Hall_(Budapest)", 0));
-        places.add(new Place("Fisherman's Bastion", "historical building", "https://en.wikipedia.org/wiki/Fisherman%27s_Bastion", 0));
         places.add(new Place("Buda Castle", "historical building", "https://en.wikipedia.org/wiki/Buda_Castle", 0));
         places.add(new Place("Hungarian Parliament Building", "historical building", "https://en.wikipedia.org/wiki/Hungarian_Parliament_Building", 0));
         places.add(new Place("Memento Park", "open-air museum", "https://en.wikipedia.org/wiki/Memento_Park", 0));
