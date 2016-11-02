a1911d536e08fefe9caa45ee93cd3098561cdf53
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-11-02 17:07:55 +0100

    Add see_buda_castle, see_budapest_history_museum, see_hungarian_national_gallery, see_varhegy

--- app/src/main/java/se/sugarest/jane/budapest/SeeFragment.java
+++ app/src/main/java/se/sugarest/jane/budapest/SeeFragment.java
@@ -25,9 +25,12 @@ public class SeeFragment extends Fragment {
         //Create an array list of places.
         ArrayList<Place> places = new ArrayList<Place>();
 
+        places.add(new Place("Várhegy", "Castle Hill", "https://en.wikipedia.org/wiki/V%C3%A1rhegy", R.drawable.see_varhegy));
+        places.add(new Place("Buda Castle", "The castle of the Hungarian kings, which was first completed in 1265.", "http://www2.budavar.hu/index.php?menu=136", R.drawable.see_buda_castle));
+        places.add(new Place("Budapest History Museum", "The museum of the capital's history documents, collect relics.", "http://www.btm.hu/", R.drawable.see_budapest_history_museum));
+        places.add(new Place("Hungarian National Gallery", "Established in 1957 as the national art museum.", "http://mng.hu/", R.drawable.see_hungarian_national_gallery));
         places.add(new Place("Budapest Zoo and Botanical Garden", "zoo & park", "https://en.wikipedia.org/wiki/Budapest_Zoo_and_Botanical_Garden", 0));
         places.add(new Place("Margaret Island", "island & park", "https://en.wikipedia.org/wiki/Margaret_Island", 0));
-        places.add(new Place("Hungarian National Gallery", "art museum", "https://en.wikipedia.org/wiki/Hungarian_National_Gallery", 0));
         places.add(new Place("Chain Bridge", "bridge", "https://en.wikipedia.org/wiki/Chain_Bridge_(Budapest)", 0));
         places.add(new Place("Deák Ferenc square", "square & transport junction", "https://en.wikipedia.org/wiki/De%C3%A1k_Ferenc_t%C3%A9r", 0));
         places.add(new Place("Great Market Hall", "indoor market", "https://en.wikipedia.org/wiki/Great_Market_Hall_(Budapest)", 0));
